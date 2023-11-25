2362cfbee875518b20d3ef9e42550a4e788dc2dd
Макс Безбородов <msbezborodov@greenatom.ru>
2023-11-25 09:00:10 +0300

    Добавлены функции для создания и проверки токена

--- common/utils.py
+++ common/utils.py
@@ -1,11 +1,17 @@
+import datetime
+import json
 import smtplib
 from contextlib import contextmanager
 from email.mime.text import MIMEText
+from functools import wraps
 from smtplib import SMTPResponseException, SMTPServerDisconnected
+from typing import Any, Callable
 
+from flask import make_response, session
+from jwt import ExpiredSignatureError, PyJWT
 from loguru import logger
 
-from config import UserConfig
+from config import Config, UserConfig
 
 
 @contextmanager
@@ -67,3 +73,70 @@ def send_email_message(email_server: smtplib.SMTP, username: str, user_email: st
     msg["Subject"] = 'Обратная связь'
     email_server.sendmail(UserConfig.SENDER, user_email, msg.as_string())
     logger.info(f"Сообщение для {user_email} было доставлено!")
+
+
+def generate_token(user_id: Any) -> str:
+    """
+    Генерирует JWT токен для пользователя.
+
+    Args:
+        user_id (Any): Уникальный идентификатор пользователя.
+
+    Returns:
+        str: Сгенерированный JWT токен.
+    """
+    payload = {
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+        'iat': datetime.datetime.utcnow(),
+        'sub': str(user_id)
+    }
+    jwt_instance = PyJWT()
+    return jwt_instance.encode(
+        payload,
+        Config.SECRET_KEY,
+        algorithm='HS256'
+    )
+
+
+def token_required(f: Callable) -> Callable:
+    """
+    Декоратор для проверки JWT токена в сессии пользователя.
+
+    Args:
+        f (Callable): Функция, к которой применяется декоратор.
+
+    Returns:
+        Callable: Обернутая функция с проверкой токена.
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = session.get('token')
+        if not token:
+            response = json.dumps(
+                {
+                    'message': 'Отсутствует токен!'
+                },
+                ensure_ascii=False
+            )
+            return make_response(response, 401, {"Content-Type": "application/json"})
+        try:
+            jwt_instance = PyJWT()
+            jwt_instance.decode(token, Config.SECRET_KEY, algorithms=['HS256'])
+        except ExpiredSignatureError:
+            response = json.dumps(
+                {
+                    'message': 'Токен просрочен!'
+                },
+                ensure_ascii=False
+            )
+            return make_response(response, 401, {"Content-Type": "application/json"})
+        except:
+            response = json.dumps(
+                {
+                    'message': 'Недействительный токен!'
+                },
+                ensure_ascii=False
+            )
+            return make_response(response, 401, {"Content-Type": "application/json"})
+        return f(*args, **kwargs)
+    return decorated_function
